68637ba866c883fbbd8d8f09fb84da78cfb28007
David Simandl <david.simandl@gmail.com>
2014-03-05 14:41:00 -0500

    Initial commit

---
+++ scraper_app/settings.py
@@ -0,0 +1,12 @@
+BOT_NAME = 'livingsocial'
+
+SPIDER_MODULES = ['scraper_app.spiders']
+
+ITEM_PIPELINES = ['scraper_app.pipelines.LivingSocialPipeline']
+
+DATABASE = {'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': '',
+            'password': '',
+            'database': 'scrape'}
